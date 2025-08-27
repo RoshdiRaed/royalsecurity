a2cfc36b974bd5eb1d25efda9cf99b56d8743baf
roshdiraed <roshdi013@gmail.com>
2025-08-27 15:04:11 +0300

    v2.6.1.4

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run()
+    {
+        // تحقق إذا المستخدم موجود مسبقًا
+        if (!User::where('email', 'admin@gmail.com')->exists()) {
+            User::create([
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('1234'), // غيرها لكلمة سر قوية
+                'is_admin' => true, // إذا عندك عمود يميز المشرفين
+            ]);
+        }
+    }
+}
